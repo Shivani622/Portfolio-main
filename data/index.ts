323bff49177a3c1440dde7ccf4ff98c4525c1f1e
Shivani622 <shivani080202@gmail.com>
2024-10-21 20:19:44 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,199 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Achievements", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently looking for FullTime Opportunities",
+    // description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "SmartHire Portal",
+    des: "Developed the SmartHire Portal with the MERN stack, enabling secure user registration, job posting, and a smart job search feature that matches profiles with job descriptions.",
+    img: "/p1.svg",
+    iconLists: ["/next.svg", "/mongo.svg", "/ts.svg", "/re.svg"],
+    // link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "GeoCall - Video Conferencing App",
+    des: "Simplify your video conferencing experience with GeoCall. Seamlessly connect with colleagues and friends.",
+    img: "/p2.svg",
+    iconLists: ["/re.svg"],
+    // link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Voice Product Recommendation",
+    des: "A voice-activated product recommendation system with 85% accuracy using BERT embeddings and a full-stack application",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/c.svg"],
+    // link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Face Mask Detection ",
+    des: " TensorFlow-based CNN model with high test accuracy, effectively managing dataset acquisition and preprocessing from Kaggle",
+    img: "/p4.svg",
+    iconLists: ["/gsap.svg"],
+    // link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Selected for Amazon’s ML Summer School, an exclusive program led by scientists and experts in ML at Amazon.",
+    name: "Amazon",
+    title: "Apprenticeship",
+  },
+  {
+    quote:
+      "Selected for Grace Hopper Celebration, an exclusive program for women and nonbinary people.",
+    name: "Grace Hopper Celebration",
+    title: "Scholarship",
+  },
+  {
+    quote:
+      "Selected as one of 30 students for Google Cloud Camp, receiving exclusive Google Cloud merchandise.",
+    name: "Google Cloud Camp",
+    title: "Boot Camp",
+  },
+  {
+    quote:
+      "Ranked top 1% in All India Joint JEE Main and Advanced 2021 out of 2.17 million candidates.",
+    name: "JEE Main and Advanced",
+    title: "Competitive Exam",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Training And Placement Cell",
+    desc: "Developed a responsive website design to ensure compatibility across various devices, ensuring that club information was accessible to a wide audience. Ensured an intuitive and user-friendly website interface, with an emphasis on UI/UX design principles.",
+    className: "md:col-span-2",
+    // thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Finance Club",
+    desc: "Created a responsive website design for multi-device compatibility, ensuring easy access to club information. Prioritized an intuitive user interface and UI/UX design principles to enhance the overall user experience.",
+    className: "md:col-span-2", // change to md:col-span-2
+    // thumbnail: "/exp2.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/Shivani622",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/shiva210164",
+  },
+];
